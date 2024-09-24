28269c135bff0b17c0bb353cd8733b44b414f894
GOBIND SINGH <129926018+A-vee@users.noreply.github.com>
2024-09-24 14:40:32 +0530

    .

---
+++ 1-d_Arrays/postloader.js
@@ -0,0 +1,28 @@
+
+function loader()
+{
+    var preloader=document.querySelectorAll(".postloader")
+    var loader=document.querySelectorAll("#loader #span")
+    var count=0;
+    const interval=setInterval(()=>
+    {
+    
+    gsap.to(
+        loader, {
+        width: `${count}%`,
+        duration: 0.5,
+        ease: "power.out",
+    })
+    count+=1;
+    if(count==100)
+        {
+            gsap.to(preloader,{
+                opacity:0
+                    })
+        }
+    },50)
+    
+}
+   
+
+window.onload=loader;
